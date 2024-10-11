3bf2d04da86d9457d9c571c74a2387f7752b0beb
bdyong <qkdeodyd00@naver.com>
2024-10-11 15:16:03 +0900

    chap 01 sec03 exam01

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+public class Hello {
+
+/*
+ 작성자 : 방대용
+ 작성일 : 2025.12 .31
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello, Java");
+	}
+
+}
