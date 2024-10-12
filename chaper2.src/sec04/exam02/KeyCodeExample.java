206be3671eb251d003a379d91c8cb18bd0322062
bdyong <qkdeodyd00@naver.com>
2024-10-12 12:15:11 +0900

    Add files via upload

---
+++ chaper2.src/sec04/exam02/KeyCodeExample.java
@@ -0,0 +1,20 @@
+package sec04.exam02;
+
+public class KeyCodeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int keyCode;
+	
+	keyCode = System.in.read();
+	System.out.println("keyCode:" + keyCode);
+	
+	keyCode = System.in.read();
+	System.out.println("keyCode:" + keyCode);
+	
+	keyCode = System.in.read();
+	System.out.println("keyCode:" + keyCode);
+
+	}
+
+}
